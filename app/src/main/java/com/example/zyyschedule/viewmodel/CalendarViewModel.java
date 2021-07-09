d2e1b64f3903d419a8905995e51843465e5f0e89
wyt618 <1965340920@qq.com>
2021-07-09 11:13:22 +0800

    创建可伸缩日历，更改calendar视图背景，实现本日跳转按钮

--- app/src/main/java/com/example/zyyschedule/viewmodel/CalendarViewModel.java
+++ app/src/main/java/com/example/zyyschedule/viewmodel/CalendarViewModel.java
@@ -2,6 +2,23 @@ package com.example.zyyschedule.viewmodel;
 
 import androidx.lifecycle.ViewModel;
 
+import java.util.Calendar;
+
 public class CalendarViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private int day ;
+
+    public CalendarViewModel() {
+        Calendar calendar=Calendar.getInstance();
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
 }
