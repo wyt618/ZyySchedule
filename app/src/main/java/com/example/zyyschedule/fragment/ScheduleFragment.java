1d903b231f9236b621bb352d4ed83bae59167649
Z <2625234729@qq.com>
2021-07-15 15:09:08 +0800

    增加悬浮按钮 侧边栏修改

--- app/src/main/java/com/example/zyyschedule/fragment/ScheduleFragment.java
+++ app/src/main/java/com/example/zyyschedule/fragment/ScheduleFragment.java
@@ -1,22 +1,28 @@
 package com.example.zyyschedule.fragment;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.ui.AppBarConfiguration;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.zyyschedule.R;
+import com.example.zyyschedule.databinding.ScheduleFragmentBinding;
 import com.example.zyyschedule.viewmodel.ScheduleViewModel;
 
 public class ScheduleFragment extends Fragment {
-
+    private ScheduleFragmentBinding binding;
     private ScheduleViewModel mViewModel;
 
     public static ScheduleFragment newInstance() {
@@ -26,8 +32,23 @@ public class ScheduleFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.schedule_fragment, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.schedule_fragment,container,false);
+
+
+        binding.ivMainMenu.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.ivMainMenu:
+                    binding.drawerLayout.openDrawer(Gravity.START);
+                    break;
+                }
+            }
+        });
+        return binding.getRoot();
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
