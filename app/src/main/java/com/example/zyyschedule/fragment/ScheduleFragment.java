782a1e40232e3f523001523b826121fc4b645325
wyt618 <1965340920@qq.com>
2021-07-19 14:07:04 +0800

    修改日程页面titlebar，新增今日日程显示fragment

--- app/src/main/java/com/example/zyyschedule/fragment/ScheduleFragment.java
+++ app/src/main/java/com/example/zyyschedule/fragment/ScheduleFragment.java
@@ -1,27 +1,25 @@
 package com.example.zyyschedule.fragment;
 
-import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.ViewModelProvider;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.ui.AppBarConfiguration;
-
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.zyyschedule.R;
 import com.example.zyyschedule.databinding.ScheduleFragmentBinding;
 import com.example.zyyschedule.viewmodel.ScheduleViewModel;
+import com.google.android.material.navigation.NavigationView;
 
-public class ScheduleFragment extends Fragment {
+public class ScheduleFragment extends Fragment implements View.OnClickListener{
     private ScheduleFragmentBinding binding;
     private ScheduleViewModel mViewModel;
 
@@ -33,17 +31,17 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater,R.layout.schedule_fragment,container,false);
-
-
-        binding.ivMainMenu.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("WrongConstant")
+        binding.ivMainMenu.setOnClickListener(this);
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                switch (v.getId()){
-                    case R.id.ivMainMenu:
-                    binding.drawerLayout.openDrawer(Gravity.START);
-                    break;
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.today:
+                        gotoTodayScheduleFragment();
+                        break;
+
                 }
+                return true;
             }
         });
         return binding.getRoot();
@@ -54,7 +52,26 @@ public class ScheduleFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(ScheduleViewModel.class);
-        // TODO: Use the ViewModel
+
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+                    case R.id.ivMainMenu:
+                    binding.drawerLayout.openDrawer(Gravity.START);
+                    break;
+                }
+    }
+
+    private void gotoTodayScheduleFragment(){
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.setTransition(FragmentTransaction.TRANSIT_NONE);
+        ft.replace(R.id.scheduleFragment, new TodayScheduleFragment(), null)
+                .commit();
+
+
+    }
+
 
 }
